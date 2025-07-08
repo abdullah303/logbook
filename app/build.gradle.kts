e5854789f88ac2fa623e802f8425841e667fbd67
abdullah303 <abdullah-ahmed@live.co.uk>
2025-07-08 18:21:32 +0100

    updated for material 3 exprssive

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,22 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     
+    // additional compose dependencies
+    implementation(libs.androidx.compose.animation)
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.foundation.layout)
+    implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.compose.material3.adaptive)
+    implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.runtime)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.text)
+    implementation(libs.androidx.savedstate.ktx)
+    implementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.graphics.shapes)
+    
     // hilt dependencies
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
